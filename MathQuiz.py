0bd43ea196f989a544e76e56a1878247bcdd2105
OnslowMitchellK <mitchell.kan@student.onslow.school.nz>
2022-06-07 12:39:11 +1200

    first save

---
+++ MathQuiz.py
@@ -0,0 +1,7 @@
+##
+# Math Quiz
+# 7/6/2022
+# Mitchell Kan
+# This programme is a math quiz game where the user gets asked a math question
+# and the user has to guess right or they lose points. There is also random
+# bonus multipliers to help the user reach 100 points faster in order to win.
